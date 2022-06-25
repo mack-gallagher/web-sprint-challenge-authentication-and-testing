6b514f2b7eaff1366b8f575489d2da897cb770cc
Mack Gallagher <wingsonthebus@gmail.com>
2022-06-24 23:49:53 -0500

    augment error messages to pass teacher tests

--- api/middleware/additional-middleware.js
+++ api/middleware/additional-middleware.js
@@ -11,7 +11,7 @@ function validate_registering_user(req, res, next) {
         if (Object.entries(req.body).length !== 2
             || !req.body.hasOwnProperty('username')
             || !req.body.hasOwnProperty('password')) {
-          res.status(400).json({ message: 'Registering users must have a username and a password, and ONLY a username and a password [???]' });
+          res.status(400).json({ message: ' [ username and password required ] Registering users must have a username and a password, and ONLY a username and a password [???]' });
           return;
         }
 
@@ -25,14 +25,14 @@ function validate_logging_in_user(req, res, next) {
   User.get_all()
     .then(result => {
       if (!result.some(x => x.username === req.body.username)) {
-        res.status(404).json({ message: 'Sorry, that user does not exist' });
+        res.status(404).json({ message: ' [ invalid credentials ] Sorry, that user does not exist' });
         return;
       } else {
 
           if (Object.entries(req.body).length !== 2
               || !req.body.hasOwnProperty('username')
               || !req.body.hasOwnProperty('password') ) {
-            res.status(400).json({ message: 'Registering users must provide a username and a password, and ONLY a username and a password [???]' });
+            res.status(400).json({ message: ' [ username and password required ] Registering users must provide a username and a password, and ONLY a username and a password [???]' });
             return;
           }
 
